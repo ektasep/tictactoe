b2a8803dff40ae7ef37d213630efc7ed515a050f
ekta saini <ektasep052001@gmail.com>
2022-06-10 20:26:33 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App () => {
+  return (
+    <>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <small> this is ekta saini</small>
+    </>
+  );
+};
+export default App;
